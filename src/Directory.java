f644071d2f04cf4a7943880de90d78f6d893188d
elr0p3 <ropefalora@gmail.com>
2020-05-01 01:18:12 +0200

    Initialized Directory class

---
+++ src/Directory.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.HashMap; 
+import java.util.Map; 
+
+public class Directory {
+
+    private String name     = "";
+    private Float size      = 0;
+    private Set<Directory> dirs_content;
+    private HashMap<String, Float> files_content;
+
+    public Directory (String name) {
+        this.name       = name
+        dirs_content    = new HashSet<Directory>();
+        files_content   = new HashMap<String, Float>();
+    }
+
+}
