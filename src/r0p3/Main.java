4b9825ba069d8b52d7b58314fe92414234c13432
elr0p3 <ropefalora@gmail.com>
2020-05-04 14:42:56 +0200

    Rename files && tested a total size getter

--- src/r0p3/Main.java
+++ src/r0p3/Main.java
@@ -6,9 +6,8 @@ public class Main {
 
     public static void main (String [] args) throws IOException {
 
-        FileSystem fs = new FileSystem();
+        SystemFile fs = new SystemFile();
         fs.travelDirectories("./");
-
     }
-
+        
 }
